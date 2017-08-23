77f4ec91510d1405ccf55d5fa8f38db2b01191f9
Cameron Green <cgree78@gmail.com>
2017-08-22 21:40:25 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+  const url = "http://45.79.7.100:8080/api/API"
+  let list = document.querySelector('.teamList')
+  try {
+    fetch(url)
+      .then((response) => {
+        Promise.resolve(response.json()).then((res) => {
+          res = JSON.parse(res)
+          for (i in res) {
+            console.log(list)
+            var ul = document.createElement('ul')
+            let li1 = document.createElement('li')
+            let li2 = document.createElement('li')
+            li1.innerHTML = res[i].Team1 + " " + res[i].Team1Score
+            li2.innerHTML = res[i].Team2 + " " + res[i].Team2Score
+            ul.innerText = "Match:"
+            ul.appendChild(li1)
+            ul.appendChild(li2)
+            list.appendChild(ul)
+          }
+        })
+      })
+      .catch((err) => {
+        console.log("Fetch Error:", err)
+      })
+  } catch (e) {
+    console.log("Couldn't use fetch")
+  }
+}
